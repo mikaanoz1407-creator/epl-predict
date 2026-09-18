e1e1bdfdbed402b2a42c7449a0be496acfac5eb9
mikaanoz1407-creator <mikaanoz1407@gmail.com>
2026-09-18 21:35:42 +0000

    Fix default export in app/dashboard/page.js

---
+++ app/dashboard/page.js
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import HomePage from '../page'; // If your component is exported from app/page.js
+
+export default function DashboardPage() {
+  return <HomePage />;
+}
